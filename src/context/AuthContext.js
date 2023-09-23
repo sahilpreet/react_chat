c14de323cdc3d7bc50ded05d35d92340170bdcc3
sahilpreet singh <sahilpreetsingh9915545834@gmail.com>
2023-09-23 00:11:00 -0400

    15 commit

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,25 +3,25 @@ import AuthReducer from "./AuthReducer";
 import { io } from "socket.io-client";
 
 const INITIAL_STATE = {
-  // user: null,
-  user: {
-    _id: "6505111c534393043829dc2c",
-    username: "sahil",
-    email: "sahil1@gmail.com",
-    password: "Sahil99155",
-    coverPicture: "",
-    followers: [
-      "6505133d15e3cef7adfbd099"
-      ],
-    followings: [
-      "6505133d15e3cef7adfbd099",
-      "650631986ad76d669eeaab99"
-      ],
-    isAdmin: false,
-    createdAt: "2023-09-11T00:42:45.572Z",
-    updatedAt: "2023-09-14T00:19:18.490Z",
-    __v: 0,
-  },
+  user: null,
+  // user: {
+  //   _id: "6505111c534393043829dc2c",
+  //   username: "sahil",
+  //   email: "sahil1@gmail.com",
+  //   password: "Sahil99155",
+  //   coverPicture: "",
+  //   followers: [
+  //     "6505133d15e3cef7adfbd099"
+  //     ],
+  //   followings: [
+  //     "6505133d15e3cef7adfbd099",
+  //     "650631986ad76d669eeaab99"
+  //     ],
+  //   isAdmin: false,
+  //   createdAt: "2023-09-11T00:42:45.572Z",
+  //   updatedAt: "2023-09-14T00:19:18.490Z",
+  //   __v: 0,
+  // },
   isFetching: false,
   error: false,
   searchItems: [],
